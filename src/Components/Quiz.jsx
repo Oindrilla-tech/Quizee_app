181a7a7cb339b07c722947c6c0f34a8380f4f061
Oindrilla-tech <oishee1820@gmail.com>
2022-07-31 22:50:13 +0530

    added

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -14,8 +14,17 @@ const Quiz = () => {
           </p>
           </div>
           <div className="qstn">
-          <p style={{"font-size":"0.7em","font-weight":"600","color":"gray","padding":"0.5em"}}>Technology</p>
+          <p style={{"font-size":"0.8em","font-weight":"600","color":"gray","padding":"0.5em"}}>Technology</p>
           <p>What is the first product launched by apple?</p>
+          <div className='options'>
+            <form>
+            <input type="button" value="iPhone" >iPhone</input>
+            <input type="button" value="iPad" >iPad </input>
+            <input type="button" value="Apple" >Apple </input>
+            <input type="button" value="iPod" >iPod </input>
+            </form>
+            
+          </div> 
         </div>
       </div>
     </div>
